2975dd28880127daf1ba608a1b4553bb28b0929b
Oleg <olegkron.cc@gmail.com>
2024-10-04 00:34:40 +0100

    init

---
+++ packages/hardhat/tasks/index.ts
@@ -0,0 +1,11 @@
+import callFunction from "./callFunction";
+import changeOwnership from "./changeOwnership";
+import upgradeProxyImplementation from "./upgradeProxyImplementation";
+import test from "./test";
+
+export default {
+  test,
+  callFunction,
+  changeOwnership,
+  upgradeProxyImplementation,
+};

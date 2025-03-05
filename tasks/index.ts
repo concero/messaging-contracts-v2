1a9e0324994864cef971c90955965255ca4d26a1
Oleg <olegkron.cc@gmail.com>
2025-03-05 22:59:18 +0000

    updates

--- tasks/index.ts
+++ tasks/index.ts
@@ -1,8 +1,8 @@
+import * as clf from "./clf";
+import { buildClfJs } from "./clf/buildClfJs";
+import { deployContracts } from "./deployContracts";
 import * as deployRouter from "./deployRouter";
 import * as deployVerifier from "./deployVerifier";
-import * as clf from "./clf";
 import * as utils from "./utils";
-import { deployContracts } from "./deployContracts";
-import { buildClfJs } from "./clf/buildClfJs";
 
 export { deployRouter, deployVerifier, clf, utils, deployContracts, buildClfJs };

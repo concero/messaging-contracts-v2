8843137f85ca4f76e01ff88e622ad371c2060e85
Oleg <olegkron.cc@gmail.com>
2025-04-24 17:15:08 +0100

    eth_call_test

--- tasks/test.ts
+++ tasks/test.ts
@@ -1,10 +1,7 @@
 import { task } from "hardhat/config";
 
-import { conceroNetworks } from "../constants/conceroNetworks";
-
 task("test-script", "A test script").setAction(async taskArgs => {
 	console.log(hre.network.name);
-	const chain = conceroNetworks[hre.network.name];
 
 	console.log("Running test-script");
 });

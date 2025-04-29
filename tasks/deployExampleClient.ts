5dce7ec550bb14e900dfe552b85c666a73a47dbe
lufaque <95128636+lufaque@users.noreply.github.com>
2025-04-29 11:02:32 +0300

    fix: update deployment configurations and router addresses for various networks

--- tasks/deployExampleClient.ts
+++ tasks/deployExampleClient.ts
@@ -2,9 +2,6 @@ import { task } from "hardhat/config";
 
 import { deployConceroClientExample } from "../deploy";
 
-/**
- * Sends a Concero message using the ConceroClientExample contract
- */
 task("deploy-example-client", "").setAction(async (taskArgs, hre) => {
 	await deployConceroClientExample(hre);
 });

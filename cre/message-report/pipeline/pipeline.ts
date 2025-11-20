d99206419f37a693d13d7ca4f3783e335ecfa9cf
lufaque <95128636+lufaque@users.noreply.github.com>
2025-11-20 13:42:55 +0300

    add cre todos

--- cre/message-report/pipeline/pipeline.ts
+++ cre/message-report/pipeline/pipeline.ts
@@ -1,43 +1,49 @@
-import {HTTPPayload, Report, Runtime} from "@chainlink/cre-sdk";
-import {sha256} from "viem";
+import { HTTPPayload, Report, Runtime } from "@chainlink/cre-sdk";
+import { sha256 } from "viem";
 
-import {DecodedArgs, DomainError, ErrorCode, GlobalConfig} from "../helpers";
-import {ChainsManager, PublicClient} from "../systems";
-import {DeploymentsManager} from "../systems/deploymentsManager";
-import {decodeArgs} from "./decodeArgs";
-import {fetchLogByMessageId} from "./fetchLogByMessageId";
-import {sendReportsToRelayer,} from "./sendReportsToRelayer";
-import {validateDecodedArgs} from "./validateDecodedArgs";
+import { DecodedArgs, DomainError, ErrorCode, GlobalConfig } from "../helpers";
+import { ChainsManager, PublicClient } from "../systems";
+import { DeploymentsManager } from "../systems/deploymentsManager";
+import { decodeArgs } from "./decodeArgs";
+import { fetchLogByMessageId } from "./fetchLogByMessageId";
+import { sendReportsToRelayer } from "./sendReportsToRelayer";
+import { validateDecodedArgs } from "./validateDecodedArgs";
 
-async function fetchReport (runtime: Runtime<GlobalConfig>, item: DecodedArgs['batch'][number]): Promise<Report> {
-    const routerAddress = DeploymentsManager.getDeploymentByChainSelector(
-        item.srcChainSelector,
-    );
-    runtime.log(`Got routerAddress=${routerAddress}`);
-    const publicClient = PublicClient.create(runtime, item.srcChainSelector);
+async function fetchReport(
+	runtime: Runtime<GlobalConfig>,
+	item: DecodedArgs["batch"][number],
+): Promise<Report> {
+	const routerAddress = DeploymentsManager.getDeploymentByChainSelector(item.srcChainSelector);
+	runtime.log(`Got routerAddress=${routerAddress}`);
+	const publicClient = PublicClient.create(runtime, item.srcChainSelector);
 
-    const log = await fetchLogByMessageId(
-        runtime,
-        publicClient,
-        routerAddress,
-        item.messageId,
-        BigInt(Number(item.blockNumber)),
-    );
-    runtime.log(`Got ConceroMessageSent Log`);
+	const log = await fetchLogByMessageId(
+		runtime,
+		publicClient,
+		routerAddress,
+		item.messageId,
+		BigInt(Number(item.blockNumber)),
+	);
+	runtime.log(`Got ConceroMessageSent Log`);
 
-    if (!log || !log?.data) {
-        runtime.log(`No log found for messageId=${item.messageId}`);
-        throw new DomainError(ErrorCode.EVENT_NOT_FOUND);
-    }
+	// TODO: In order to support the wait for a certain
+	//  number of block confirmations, we need to make a parallel request for the current block number and see if there are enough block confirmations.
 
-    return runtime
-        .report({
-            encoderName: "evm",
-            encodedPayload: sha256(log.data),
-            signingAlgo: "ecdsa",
-            hashingAlgo: "keccak256",
-        })
-        .result();
+	// TODO: Validate which validator library is specified in the event. If it is not a CRE validator library, do not create a report
+	if (!log || !log?.data) {
+		runtime.log(`No log found for messageId=${item.messageId}`);
+		// TODO: We are batching, so we should not throw an error if we did not find 1 event from the batch.
+		throw new DomainError(ErrorCode.EVENT_NOT_FOUND);
+	}
+
+	return runtime
+		.report({
+			encoderName: "evm",
+			encodedPayload: sha256(log.data),
+			signingAlgo: "ecdsa",
+			hashingAlgo: "keccak256",
+		})
+		.result();
 }
 
 // pipeline stages for each validation request
@@ -51,11 +57,11 @@ export async function pipeline(runtime: Runtime<GlobalConfig>, payload: HTTPPayl
 		validateDecodedArgs(args);
 		runtime.log(`Decoded args: ${JSON.stringify(args)}`);
 
-        const fetchReportPromises = args.batch.map(item => fetchReport(runtime, item))
-        const reports = await Promise.all(fetchReportPromises)
+		const fetchReportPromises = args.batch.map(item => fetchReport(runtime, item));
+		const reports = await Promise.all(fetchReportPromises);
 		sendReportsToRelayer(runtime, reports);
 
-        // @todo: check that should respond with batches
+		// @todo: check that should respond with batches
 		return sha256(payload.input);
 	} catch (error) {
 		runtime.log(

7fa15161a3dea3f82f14cf1aa64d4531348e84a0
d <roman@concero.io>
2025-12-10 17:33:35 +0300

    [fix]: cre blockConfirmations validation

--- cre/message-report/pipeline/validateBlockConfirmations.ts
+++ cre/message-report/pipeline/validateBlockConfirmations.ts
@@ -1,8 +1,6 @@
 import { DecodedMessageSentReceipt, DomainError, ErrorCode } from "../helpers";
 import { ChainsManager } from "../systems";
 
-const UINT64_MAX = 18446744073709551615n;
-
 export const validateBlockConfirmations = (
 	logBlockNumber: bigint,
 	logParsedReceipt: DecodedMessageSentReceipt,
@@ -12,16 +10,13 @@ export const validateBlockConfirmations = (
 		return;
 	}
 
-	let finalityBlockConfirmations: bigint = 0n;
-	if (logParsedReceipt.srcChainData.blockConfirmations !== UINT64_MAX) {
-		finalityBlockConfirmations = logParsedReceipt.srcChainData.blockConfirmations;
-	} else {
-		finalityBlockConfirmations = BigInt(
-			ChainsManager.getOptionsBySelector(logParsedReceipt.srcChainSelector)
-				.finalityConfirmations ?? 0,
-		);
-	}
-	if (logBlockNumber + finalityBlockConfirmations < currentChainBlockNumber) {
-		throw new DomainError(ErrorCode.UNKNOWN_ERROR, "Block was not finalized ");
+	const requestBlockNumbers = Number(logParsedReceipt.srcChainData.blockConfirmations);
+	const configBlockNumber =
+		ChainsManager.getOptionsBySelector(logParsedReceipt.srcChainSelector)
+			.finalityConfirmations || 0;
+
+	const finalityBlocksDelta = Math.max(requestBlockNumbers, configBlockNumber);
+	if (logBlockNumber + BigInt(finalityBlocksDelta) < currentChainBlockNumber) {
+		throw new DomainError(ErrorCode.UNKNOWN_ERROR, "Block was not finalized");
 	}
 };

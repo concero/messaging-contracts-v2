cc459f92d2eb1a88a32f43ff3006b976e6c9e4c6
d <roman@concero.io>
2025-12-11 17:05:58 +0300

    [fix]: fixed blockConformations check fix

--- cre/message-report/pipeline/validateBlockConfirmations.ts
+++ cre/message-report/pipeline/validateBlockConfirmations.ts
@@ -1,6 +1,8 @@
 import { DecodedMessageSentReceipt, DomainError, ErrorCode } from "../helpers";
 import { ChainsManager } from "../systems";
 
+const UINT64_MAX = 18446744073709551615n;
+
 export const validateBlockConfirmations = (
 	logBlockNumber: bigint,
 	logParsedReceipt: DecodedMessageSentReceipt,
@@ -10,16 +12,15 @@ export const validateBlockConfirmations = (
 		return;
 	}
 
-	const requestedBlockConfirmations = logParsedReceipt.srcChainData.blockConfirmations;
-	const finalityBlockConfirmations = BigInt(
-		ChainsManager.getOptionsBySelector(logParsedReceipt.srcChainSelector)
-			.finalityConfirmations || 0,
-	);
-
-	const blockConfirmationsDelta =
-		requestedBlockConfirmations > finalityBlockConfirmations
-			? requestedBlockConfirmations
-			: finalityBlockConfirmations;
+	let blockConfirmationsDelta: bigint = 0n;
+	if (logParsedReceipt.srcChainData.blockConfirmations === UINT64_MAX) {
+		blockConfirmationsDelta = logParsedReceipt.srcChainData.blockConfirmations;
+	} else {
+		blockConfirmationsDelta = BigInt(
+			ChainsManager.getOptionsBySelector(logParsedReceipt.srcChainSelector)
+				.finalityConfirmations ?? 0,
+		);
+	}
 
 	if (logBlockNumber + blockConfirmationsDelta < currentChainBlockNumber) {
 		throw new DomainError(ErrorCode.UNKNOWN_ERROR, "Not enough block confirmations");

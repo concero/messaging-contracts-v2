b794b07065c309de5c8f2b517962bded4976ffed
lufaque <95128636+lufaque@users.noreply.github.com>
2026-02-09 20:45:40 +0300

    deploy routers

--- tasks/deployMessagingToChain.task.ts
+++ tasks/deployMessagingToChain.task.ts
@@ -8,20 +8,36 @@ import { getDeployerBalance } from "./getDeployerBalance.task";
 
 import { mainnetRelayer } from "./relayer/setRelayerLibVars";
 import { sendNativeTokenTrezor } from "./nativeSender/sendNativeTokenTrezor.task";
-import { log } from "@concero/contract-utils";
+import { getEnvVar, log } from "@concero/contract-utils";
 import { formatUnits } from "viem";
+import { deployPriceFeedTask } from "./deployPriceFeed/deployPriceFeedTask";
 
 task("deploy-messaging", "Deploy ConceroRouter").setAction(
 	async (taskArgs, hre: HardhatRuntimeEnvironment) => {
+		await deployPriceFeedTask({ implementation: true, proxy: true }, hre);
+
+		let deployerBalance = await getDeployerBalance(hre);
+		let amountToSend = formatUnits((deployerBalance * 10n) / 100n, 18);
+
+		log(
+			`Deployer balance: ${formatUnits(deployerBalance, 18)}. Sending ${amountToSend} to price feed updater`,
+			"deploy-messaging",
+		);
+
+		await sendNativeTokenTrezor(
+			{ recipient: getEnvVar("MAINNET_FEED_UPDATER_ADDRESS"), amount: amountToSend },
+			hre,
+		);
+
 		await deployRouterTask({ implementation: true, proxy: true }, hre);
 		await deployCreValidatorLibTask({ implementation: true, proxy: true }, hre);
 		await deployRelayerLibTask({ implementation: true, proxy: true, vars: true }, hre);
 
-		const deployerBalance = await getDeployerBalance(hre);
-		const amountToSend = formatUnits((deployerBalance * 30n) / 100n, 18);
+		deployerBalance = await getDeployerBalance(hre);
+		amountToSend = formatUnits((deployerBalance * 30n) / 100n, 18);
 
 		log(
-			`Deployer balance: ${formatUnits(deployerBalance, 18)}. Sending ${amountToSend}`,
+			`Deployer balance: ${formatUnits(deployerBalance, 18)}. Sending ${amountToSend} to relayer`,
 			"deploy-messaging",
 		);
 		await sendNativeTokenTrezor({ recipient: mainnetRelayer, amount: amountToSend }, hre);

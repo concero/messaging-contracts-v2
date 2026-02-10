4f0897340b292c2b7caf74de8de98cddcc215f53
lufaque <95128636+lufaque@users.noreply.github.com>
2026-02-10 13:56:46 +0300

    deploy routers

--- tasks/deployMessagingToChain.task.ts
+++ tasks/deployMessagingToChain.task.ts
@@ -1,33 +1,36 @@
 import { task } from "hardhat/config";
 
 import { type HardhatRuntimeEnvironment } from "hardhat/types";
+import { deployRouterTask } from "./deployRouter/deployRouterTask";
+import { deployCreValidatorLibTask } from "./cre/deployCreValidatorLib.task";
 import { deployRelayerLibTask } from "./relayer/deployRelayer.task";
 import { getDeployerBalance } from "./getDeployerBalance.task";
 
 import { mainnetRelayer } from "./relayer/setRelayerLibVars";
 import { sendNativeTokenTrezor } from "./nativeSender/sendNativeTokenTrezor.task";
-import { log } from "@concero/contract-utils";
+import { getEnvVar, log } from "@concero/contract-utils";
 import { formatUnits } from "viem";
+import { deployPriceFeedTask } from "./deployPriceFeed/deployPriceFeedTask";
 
 task("deploy-messaging", "Deploy ConceroRouter").setAction(
 	async (taskArgs, hre: HardhatRuntimeEnvironment) => {
-		// await deployPriceFeedTask({ implementation: true, proxy: true }, hre);
+		await deployPriceFeedTask({ implementation: true, proxy: true }, hre);
 
 		let deployerBalance = await getDeployerBalance(hre);
 		let amountToSend = formatUnits((deployerBalance * 10n) / 100n, 18);
 
-		// log(
-		// 	`Deployer balance: ${formatUnits(deployerBalance, 18)}. Sending ${amountToSend} to price feed updater`,
-		// 	"deploy-messaging",
-		// );
-		//
-		// await sendNativeTokenTrezor(
-		// 	{ recipient: getEnvVar("MAINNET_FEED_UPDATER_ADDRESS"), amount: amountToSend },
-		// 	hre,
-		// );
+		log(
+			`Deployer balance: ${formatUnits(deployerBalance, 18)}. Sending ${amountToSend} to price feed updater`,
+			"deploy-messaging",
+		);
 
-		// await deployRouterTask({ implementation: true, proxy: true }, hre);
-		// await deployCreValidatorLibTask({ implementation: true, proxy: true }, hre);
+		await sendNativeTokenTrezor(
+			{ recipient: getEnvVar("MAINNET_FEED_UPDATER_ADDRESS"), amount: amountToSend },
+			hre,
+		);
+
+		await deployRouterTask({ implementation: true, proxy: true }, hre);
+		await deployCreValidatorLibTask({ implementation: true, proxy: true }, hre);
 		await deployRelayerLibTask({ implementation: true, proxy: true, vars: true }, hre);
 
 		deployerBalance = await getDeployerBalance(hre);
